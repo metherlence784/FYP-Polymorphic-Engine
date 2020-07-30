667b6fcd0b97d636dc6878ef8845593b7345e0d7
jtf256 <jtf256@hotmail.com>
2020-07-30 23:24:03 +0800

    changed the scaling factor again

--- Polymorphic_Engine_V1/main.cpp
+++ Polymorphic_Engine_V1/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR","1");
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR","0");
     a.setStyle(QStyleFactory::create("Fusion")); // to have a clean ui look
     MainWindow w;
     w.show();//run the application
